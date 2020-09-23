4abdfbb83e17e0773434dc61bf3bb1783752ac3a
Mike <ilmivy1024@gmail.com>
2020-09-23 16:30:14 +0600

    Initial Commit

---
+++ App_Mikhail_Ilyushkin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  App_Mikhail_Ilyushkin
+//
+//  Created by Michael Iliouchkin on 23.09.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
