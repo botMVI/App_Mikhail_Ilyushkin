8b863fa1ff22a1aaf62c267db4708b8e49031be9
Mike <ilmivy1024@gmail.com>
2020-09-28 21:53:59 +0600

    addingInfoFromLast

---
+++ App_Mikhail_Ilyushkin/Model/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  App_Mikhail_Ilyushkin
+//
+//  Created by Michael Iliouchkin on 28.09.2020.
+//
+
+import UIKit
+
+struct User {
+    let name: String
+    let photo: UIImage
+}
---
+++ App_Mikhail_Ilyushkin/TabbarAndInfo/Profile/Friends/FriendsList/ImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCell.swift
+//  App_Mikhail_Ilyushkin
+//
+//  Created by Michael Iliouchkin on 28.09.2020.
+//
+
+import Foundation
+
+class ImageCell: UICollectionViewCell {
+
+    @IBOutlet weak var selectedImage: UIImageView!
+
+}
